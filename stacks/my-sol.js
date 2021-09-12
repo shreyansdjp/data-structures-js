05ca00e05feee8636b66813a051527cb73feefc7
Shreyans Jain <shreyansjain68@gmail.com>
2021-09-13 01:16:38 +0530

    build: prettier

--- stacks/my-sol.js
+++ stacks/my-sol.js
@@ -1,30 +1,30 @@
 class Stack {
-  constructor() {
-    this.stackArray = [];
-    this.current = -1;
-  }
+    constructor() {
+        this.stackArray = [];
+        this.current = -1;
+    }
 
-  push(item) {
-    this.current += 1;
-    this.stackArray[this.current] = item;
-  }
+    push(item) {
+        this.current += 1;
+        this.stackArray[this.current] = item;
+    }
 
-  pop() {
-    if (this.current === -1) return null;
+    pop() {
+        if (this.current === -1) return null;
 
-    const lastItem = this.stackArray[this.current];
-    delete this.stackArray[this.current];
-    this.current -= 1;
-    return lastItem;
-  }
+        const lastItem = this.stackArray[this.current];
+        delete this.stackArray[this.current];
+        this.current -= 1;
+        return lastItem;
+    }
 
-  peek() {
-    return this.stackArray[this.current];
-  }
+    peek() {
+        return this.stackArray[this.current];
+    }
 
-  get length() {
-    return this.current + 1;
-  }
+    get length() {
+        return this.current + 1;
+    }
 }
 
 // copied results statements from video sol
