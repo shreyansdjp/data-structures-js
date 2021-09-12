40c3f63308ff2484ac3da006e3f63d8d41a8fbee
Shreyans Jain <shreyansjain68@gmail.com>
2021-09-13 01:13:47 +0530

    refact: stacks class

--- stacks/my-sol.js
+++ stacks/my-sol.js
@@ -1,21 +1,29 @@
 class Stack {
-  arr = [];
+  constructor() {
+    this.stackArray = [];
+    this.current = -1;
+  }
 
   push(item) {
-    this.arr.push(item);
-    return this.arr.length;
+    this.current += 1;
+    this.stackArray[this.current] = item;
   }
 
   pop() {
-    return this.arr.pop();
+    if (this.current === -1) return null;
+
+    const lastItem = this.stackArray[this.current];
+    delete this.stackArray[this.current];
+    this.current -= 1;
+    return lastItem;
   }
 
   peek() {
-    return this.arr[this.arr.length - 1];
+    return this.stackArray[this.current];
   }
 
-  length() {
-    return this.arr.length;
+  get length() {
+    return this.current + 1;
   }
 }
 
@@ -28,7 +36,7 @@ console.log(myStack.peek());
 console.log(myStack.pop());
 console.log(myStack.peek());
 myStack.push("freeCodeCamp");
-console.log(myStack.size());
+console.log(myStack.length);
 console.log(myStack.peek());
 console.log(myStack.pop());
 console.log(myStack.peek());
